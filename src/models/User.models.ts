358420757b8f51432c4568d58ecd0c635c4b481f
Shariq Choudhary <khanshariq92213@gmail.com>
2025-01-04 21:51:33 +0530

    User Model Completed

---
+++ src/models/User.models.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date
+}
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpires: Date;
+    isVerified: boolean;
+    isAcceptingMessages: boolean;
+    messages: Message[];
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, required: true }
+})
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String, required: true, trim: true, unique: true
+    },
+    email: { type: String, required: true, unique: true, match: [/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g, 'Please fill a valid email address'] },
+    password: { type: String, required: true },
+    verifyCode: { type: String, required: true },
+    verifyCodeExpires: { type: Date, required: true },
+    isVerified: { type: Boolean, default: false },
+    isAcceptingMessages: { type: Boolean, default: true, required: true },
+    messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }]
+})
+
+export const UserModel = mongoose.model<User>('User', UserSchema);
+export default mongoose.model<Message>('Message', MessageSchema);
